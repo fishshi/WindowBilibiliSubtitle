11fe25044df414a9aea96c1a7f7e627bea6eef4f
fishshi <2855691008@qq.com>
2024-10-12 10:32:28 +0800

    当悬浮窗开启时，油猴插件可以自动打开字幕

--- WindowBilibiliSubtitle.user.js
+++ WindowBilibiliSubtitle.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WindowBilibiliSubtitle
-// @version      1.0.0
+// @version      1.1.0
 // @description  悬浮窗显示Bilibili字幕（获取字幕内容并通过WebSocket发送至悬浮窗）
 // @author       fishshi
 // @match        https://www.bilibili.com/video/*
@@ -9,36 +9,38 @@
 
 (function () {
 	'use strict';
-	function observeSubtitle() {
-		const subtitleDiv = document.querySelector('.bpx-player-subtitle-panel-major-group');
-		if (subtitleDiv) {
-			console.log('找到字幕');
-			const ws = new WebSocket('ws://localhost:80');
-			ws.onopen = () => {
-				console.log('WebSocket 连接成功');
-			};
-			ws.onerror = (error) => {
-				console.log('WebSocket 错误: ', error);
-			};
-			const observer = new MutationObserver((mutations) => {
-				mutations.forEach((mutation) => {
-					if (mutation.type === 'childList') {
-						const subtitleSpan = document.querySelector('.bpx-player-subtitle-panel-text');
-						if (subtitleSpan) {
-							console.log('字幕更新:', subtitleSpan.textContent);
-							if (ws.readyState === WebSocket.OPEN) {
-								ws.send(subtitleSpan.textContent);
+	const ws = new WebSocket('ws://localhost:80');
+	ws.onopen = () => {
+		console.log('WebSocket 连接成功');
+		const checkInterval = setInterval(() => {
+			const subtitleCtrlDiv = document.querySelector('.bpx-player-ctrl-subtitle');
+			if (subtitleCtrlDiv) {
+				console.log("字幕按钮存在");
+				const flag = subtitleCtrlDiv.querySelector('filter');
+				if (flag) {
+					document.querySelector('.bpx-player-ctrl-btn[aria-label="字幕"] .bpx-common-svg-icon').click();
+				}
+				const subtitleDiv = document.querySelector('.bpx-player-subtitle-panel-major-group');
+				const observer = new MutationObserver((mutations) => {
+					mutations.forEach((mutation) => {
+						if (mutation.type === 'childList') {
+							const subtitleSpan = document.querySelector('.bpx-player-subtitle-panel-text');
+							if (subtitleSpan) {
+								console.log('字幕更新:', subtitleSpan.textContent);
+								if (ws.readyState === WebSocket.OPEN) {
+									ws.send(subtitleSpan.textContent);
+								}
 							}
 						}
-					}
+					});
 				});
-			});
-			const config = { subtree: true, childList: true };
-			observer.observe(subtitleDiv, config);
-			clearInterval(checkInterval);
-		} else {
-			console.log('未找到字幕');
-		}
-	}
-	const checkInterval = setInterval(observeSubtitle, 2000);
+				const config = { subtree: true, childList: true };
+				observer.observe(subtitleDiv, config);
+				clearInterval(checkInterval);
+			}
+		}, 2000);
+	};
+	ws.onerror = (error) => {
+		console.log('WebSocket 错误: ', error);
+	};
 })();
